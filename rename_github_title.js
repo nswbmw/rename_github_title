bf11fba1a8fca52a69dbeab943ad8ebad570a7b8
nswbmw <gxqzk@126.com>
2016-02-03 21:02:32 +0800

    rm tab

--- rename_github_title.js
+++ rename_github_title.js
@@ -2,7 +2,7 @@
 (function rename_github_title() {
   'use strict';
   
-	var pathname = window.location.pathname;
+  var pathname = window.location.pathname;
   var matches = pathname.match(/^\/([^/]+)\/([^/]+)/);
   if (matches) {
     document.title = matches[2];
