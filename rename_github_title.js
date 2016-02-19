124fb25de8340365978c9dd6a5c699ab95a02c66
nswbmw <gxqzk@126.com>
2016-02-19 14:17:15 +0800

    add decodeURIComponent

--- rename_github_title.js
+++ rename_github_title.js
@@ -2,7 +2,7 @@
 (function rename_github_title() {
   'use strict';
 
-  var pathname = window.location.pathname.replace(/\/blob\/[^/]+/, '');
+  var pathname = decodeURIComponent(window.location.pathname.replace(/\/blob\/[^/]+/, ''));
   var arr = pathname.split('/');
   if (arr.length > 2) {
     document.title = arr.slice(2).join('/');
